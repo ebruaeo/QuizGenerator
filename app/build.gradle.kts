c1b74c906a81c1d9c8cc7e9ca6350b160dde1e85
ebruaeo <ebruaeo@hotmail.com>
2024-05-09 16:38:45 +0300

    viewbinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,9 @@ plugins {
 android {
     namespace = "com.example.quizgenerator"
     compileSdk = 34
-
+    buildFeatures{
+        viewBinding = true
+    }
     defaultConfig {
         applicationId = "com.example.quizgenerator"
         minSdk = 24
