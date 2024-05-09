3f0b4d67ab1de767cacfe52588719fcd9a09b9ce
ebruaeo <ebruaeo@hotmail.com>
2024-05-09 16:40:25 +0300

    start again button set onclick listener

---
+++ app/src/main/java/com/example/quizgenerator/ScoreScreen.kt
@@ -0,0 +1,29 @@
+package com.example.quizgenerator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.quizgenerator.databinding.ActivityMainBinding
+import com.example.quizgenerator.databinding.ActivityScoreScreenBinding
+
+class ScoreScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityScoreScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding= ActivityScoreScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.startAgainButton.setOnClickListener {
+            finish()
+        }
+    }
+
+}
