9d6bd6f855ffa1e3c5f3439b7328ef92229ed40e
ebruaeo <ebruaeo@hotmail.com>
2024-05-13 15:41:57 +0300

    Question data class is implemented

--- app/src/main/java/com/example/quizgenerator/MainActivity.kt
+++ app/src/main/java/com/example/quizgenerator/MainActivity.kt
@@ -15,42 +15,57 @@ class MainActivity : AppCompatActivity() {
     private val restTime = 1000L
     private var isScoreActivityStarted = false
 
-    private val questions = arrayOf(
+    // 1- Veri modellemesi
+    // 2- Akış planlaması
+
+    /*private val questions = arrayOf(
         "A slug's blood is green.",
         "Approximately one quarter of human bones are in the feet.",
         "The total surface area of two human lungs is approximately 70 square metres.",
         "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.",
-        /* "It is illegal to pee in the Ocean in Portugal.",
+        "It is illegal to pee in the Ocean in Portugal.",
          "You can lead a cow down stairs but not up stairs.",
          "Google was originally called 'Backrub'.",
          "Buzz Aldrin's mother's maiden name was 'Moon'.",
          "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.",
          "No piece of square dry paper can be folded in half more than 7 times.",
-         "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog."*/
-    )
+         "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog."
+    )*/
+    /* private val options = arrayOf(
+         arrayOf("True", "False"),
+         arrayOf("True", "False"),
+         arrayOf("True", "False"),
+         arrayOf("True", "False"),
+         /* arrayOf("True", "False"),
+          arrayOf("True", "False"),
+          arrayOf("True", "False"),
+          arrayOf("True", "False"),
+          arrayOf("True", "False"),
+          arrayOf("True", "False"),
+          arrayOf("True", "False"),*/
+     ) */
+    /* private val correctAnswers = arrayOf(
+         0, 0, 0, 0, // 0, 1, 0, 0, 1, 1, 0
+     ) */
 
-    private val options = arrayOf(
-        arrayOf("True", "False"),
-        arrayOf("True", "False"),
-        arrayOf("True", "False"),
-        arrayOf("True", "False"),
-        /* arrayOf("True", "False"),
-         arrayOf("True", "False"),
-         arrayOf("True", "False"),
-         arrayOf("True", "False"),
-         arrayOf("True", "False"),
-         arrayOf("True", "False"),
-         arrayOf("True", "False"),*/
-    )
-
-    private val correctAnswers = arrayOf(
-        0, 0, 0, 0, // 0, 1, 0, 0, 1, 1, 0
+    private val questionList = arrayOf(
+        Question(1, "A slug's blood is green.", true),
+        Question(2, "Approximately one quarter of human bones are in the feet.", true),
+        Question(
+            3,
+            "The total surface area of two human lungs is approximately 70 square metres.",
+            true
+        ),
+        Question(
+            4,
+            "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.",
+            true
+        ),
     )
 
     private var currentQuestionIndex = 0
     private var score = 0
 
-    private var isAnswered = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -86,7 +101,7 @@ class MainActivity : AppCompatActivity() {
     private fun setUpSeekBar() {
         binding.seekBar.run {
             // isEnabled = false
-            max = questions.size
+            max = questionList.size
             setOnTouchListener { view, motionEvent -> true }
         }
     }
@@ -115,17 +130,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun ActivityMainBinding.displayQuestions() {
-        quizText.text = questions[currentQuestionIndex]
-        optionOneButton.text = options[currentQuestionIndex][0]
-        optionTwoButton.text = options[currentQuestionIndex][1]
+        quizText.text = questionList[currentQuestionIndex].text
+        //optionOneButton.text = options[currentQuestionIndex][0]
+        //optionTwoButton.text = options[currentQuestionIndex][1]
         resetButtonColors()
         seekBar.progress += 1
         unfreezeButtons()
     }
 
+    // TODO Use enum class
     private fun checkAnswer(selectedAnswerIndex: Int) {
-        val correctAnswerIndex = correctAnswers[currentQuestionIndex]
-        if (selectedAnswerIndex == correctAnswerIndex) {
+        val answer = when (selectedAnswerIndex) {
+            0 -> true
+            1 -> false
+            else -> throw Exception("selectedAnswerIndex is wrong: $selectedAnswerIndex")
+        }
+        if (answer == questionList[currentQuestionIndex].answer) {
+            score++
+            binding.scoreValue.text = score.toString()
+            correctButtonColors(selectedAnswerIndex)
+        } else {
+            wrongButtonColors(selectedAnswerIndex)
+            val correctAnswerIndex = if (selectedAnswerIndex == 0) 1 else 0
+            correctButtonColors(correctAnswerIndex)
+        }
+
+        //val correctAnswerIndex = correctAnswers[currentQuestionIndex]
+        /*if (selectedAnswerIndex == correctAnswerIndex) {
             score++
             binding.scoreValue.text = score.toString()
             correctButtonColors(selectedAnswerIndex)
@@ -133,17 +164,17 @@ class MainActivity : AppCompatActivity() {
         } else {
             wrongButtonColors(selectedAnswerIndex)
             correctButtonColors(correctAnswerIndex)
-        }
+        }*/
     }
 
     private fun setNext() {
-        if (currentQuestionIndex < questions.size - 1) {
+        if (currentQuestionIndex < questionList.size - 1) {
             currentQuestionIndex++
             binding.quizText.postDelayed({ binding.displayQuestions() }, restTime)
-        } else if (currentQuestionIndex == questions.size - 1) {
+        } else if (currentQuestionIndex == questionList.size - 1) {
             val intent = Intent(this@MainActivity, ScoreActivity::class.java)
             intent.putExtra(ScoreActivity.SCORE_KEY, score)
-            intent.putExtra(ScoreActivity.QUESTION_COUNT_KEY, questions.size)
+            intent.putExtra(ScoreActivity.QUESTION_COUNT_KEY, questionList.size)
             startActivity(intent)
             isScoreActivityStarted = true
         }
---
+++ app/src/main/java/com/example/quizgenerator/Question.kt
@@ -0,0 +1,4 @@
+package com.example.quizgenerator
+
+// Data immutability
+data class Question(val id: Int, val text: String, val answer: Boolean)
